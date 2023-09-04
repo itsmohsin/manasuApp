3ea6e05f23320031f19b84f6e4bee518e86ac727
itsmohsin <mohsinsmannat@gmail.com>
2023-09-04 12:44:33 +0530

    done with configuration and constant utils

---
+++ lib/utils/text.dart
@@ -0,0 +1,14 @@
+// this i si pre-defined text for application
+
+class AppText {
+  static final enText = {
+    'welcome_text': 'Welcome',
+    'signIn_text': 'Sign in to your account',
+    'registered_text': 'Already have an account?',
+    'register_text':
+        'You can easilly sign up, and connect to the Doctors nearby',
+    'signUp_text': 'Don\'t have an account?',
+    'social-login': 'Or continue with social account',
+    'forgot_password': 'Forgot your password?',
+  };
+}
