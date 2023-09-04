99faf46a2287a92d6309b303847a32bc264a8317
itsmohsin <mohsinsmannat@gmail.com>
2023-09-04 20:02:42 +0530

    done with doctor card

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:manasu/components/appointment_card.dart';
+import 'package:manasu/components/doctor_card.dart';
 import 'package:manasu/utils/config.dart';
 
 class HomePage extends StatefulWidget {
@@ -47,94 +48,103 @@ class _HomePageState extends State<HomePage> {
           vertical: 15.0,
         ),
         child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    'Mohsin', // hard code the user's name at first
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    child: CircleAvatar(
-                      radius: 30,
-                      backgroundImage:
-                          AssetImage('assets/man.png'), // insert the image here
-                    ),
-                  )
-                ],
-              ),
-              Config.spaceSmall,
-              // category listing
-              const Text(
-                'Category', // hard code the user's name at first
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Config.spaceSmall,
-              // build category list
-              SizedBox(
-                height: Config.heightSize * 0.075,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: List<Widget>.generate(medCat.length, (index) {
-                    return Card(
-                      margin: const EdgeInsets.only(right: 15),
-                      color: Config.primaryColor,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10,
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: <Widget>[
-                            FaIcon(
-                              medCat[index]['icon'],
-                              color: Colors.white,
-                            ),
-                            const SizedBox(
-                              width: 20,
-                            ),
-                            Text(
-                              medCat[index]['category'],
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 16,
-                              ),
-                            )
-                          ],
-                        ),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Mohsin', // hard code the user's name at first
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
                       ),
-                    );
+                    ),
+                    SizedBox(
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: AssetImage(
+                            'assets/man.png'), // insert the image here
+                      ),
+                    )
+                  ],
+                ),
+                Config.spaceSmall,
+                // category listing
+                const Text(
+                  'Category', // hard code the user's name at first
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Config.spaceSmall,
+                // build category list
+                SizedBox(
+                  height: Config.heightSize * 0.075,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: List<Widget>.generate(medCat.length, (index) {
+                      return Card(
+                        margin: const EdgeInsets.only(right: 15),
+                        color: Config.primaryColor,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 15,
+                            vertical: 10,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: <Widget>[
+                              FaIcon(
+                                medCat[index]['icon'],
+                                color: Colors.white,
+                              ),
+                              const SizedBox(
+                                width: 20,
+                              ),
+                              Text(
+                                medCat[index]['category'],
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+                Config.spaceSmall,
+                const Text(
+                  'Appointment Today',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Config.spaceSmall,
+                // display appointment card here
+                const AppointmentCard(),
+                Config.spaceSmall,
+                const Text(
+                  'Top Doctors',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Config.spaceSmall,
+                // list of top doctos
+                Column(
+                  children: List.generate(10, (index) {
+                    return const DoctorCard();
                   }),
-                ),
-              ),
-              Config.spaceSmall,
-              const Text(
-                'Appointment Today',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Config.spaceSmall,
-              // display appointment card here
-              const AppointmentCard(),
-              Config.spaceSmall,
-              const Text(
-                'Top Doctors',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
